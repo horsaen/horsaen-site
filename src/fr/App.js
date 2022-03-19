9d12295f79616414eb530a3a7165f9c878aae2c2
Cameron W <atealltheglue@protonmail.com>
2022-03-19 16:36:47 -0500

    french support

--- src/fr/App.js
+++ src/fr/App.js
@@ -1,7 +1,10 @@
 import { Link } from 'react-router-dom';
 
+import Landing from "./components/Landing.js";
+import Navbar from "./components/Navbar.js";
+
 export default function AppFr () {
     return (
-        <div></div>
+        <Landing />
     );
 }
