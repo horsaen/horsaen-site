7717ada5ab8921c67a8ce064ea02ada5f9e97c3c
Cameron W <atealltheglue@protonmail.com>
2022-03-25 15:20:40 -0500

    updates

--- src/NotFound.js
+++ src/NotFound.js
@@ -1,7 +1,17 @@
 import { Link } from 'react-router-dom';
+import './NotFound.css';
 
 export default function NotFound() {
     return (
-        <Link to='/'>Home</Link>
+        <div className="page-container">
+            <div className="notfound-header">
+                <span>this site can't be reached :((</span>
+            </div>
+            <Link to="/">
+                <div className="button">
+                    <span>Return home !!</span>
+                </div>
+            </Link>
+        </div>
     )
 }
--- src/components/About.js
+++ src/components/About.js
@@ -3,6 +3,10 @@ import './About.css';
 
 export default function About () {
     return (
-        <div id="about-container" className="about-container"></div>
+        <div id="about-container" className="about-container">
+            <div className="about-header">
+                <span>About Me</span>
+            </div>
+        </div>
     )
 }
