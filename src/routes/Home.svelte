8251eccd48971c570b0bb7d9012959ba1b35fd85
horsaen <atealltheglue@protonmail.com>
2022-07-16 20:43:20 -0500

    sync

--- src/routes/Home.svelte
+++ src/routes/Home.svelte
@@ -1,9 +1,20 @@
 <main>
     <div>
-        <span></span>
+        <div class="landing-container">
+            <div class="landing-text">
+                <span class="landing-top">Hey, I'm</span>
+                <span class="landing-bottom">{"> Horsaen"}</span>
+            </div>
+        </div>
     </div>
 </main>
 
 <style>
-
+    .landing-text {
+        display: flex;
+        flex-direction: column;
+    }
+    .landing-bottom {
+        padding-left: 15px;
+    }
 </style>
