e02d046dfd741de88de11b3349f953d3af52f53c
horsaen <atealltheglue@protonmail.com>
2022-07-25 07:49:21 -0500

    basic landing

--- src/routes/Home.svelte
+++ src/routes/Home.svelte
@@ -3,7 +3,7 @@
         <div class="landing-container">
             <div class="landing-text">
                 <span class="landing-top">Hey, I'm</span>
-                <span class="landing-bottom">{"> Horsaen"}</span>
+                <span class="landing-bottom">{"> Cameron"}<span class="cursor">|</span></span>
             </div>
         </div>
     </div>
@@ -11,10 +11,31 @@
 
 <style>
     .landing-text {
+        position: absolute;
+        left: 50px;
+        bottom: 50px;
         display: flex;
         flex-direction: column;
+        font-size: 76px;
+    }
+    .landing-top {
+        padding-right: 300px;
     }
     .landing-bottom {
-        padding-left: 15px;
+        display: flex;
+        background-color: #fff;
+        color: #000;
+        width: fit-content;
+    }
+    .cursor {
+        animation: 1.5s blink step-end infinite;
+    }
+    @keyframes blink {
+        from, to {
+            color: transparent;
+        }
+        50% {
+            color: #000;
+        }
     }
 </style>
